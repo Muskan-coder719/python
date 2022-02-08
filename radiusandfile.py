9a7cad5c0faa12940318edd4063db7bf553b1b1f
Muskan-coder719 <82265381+Muskan-coder719@users.noreply.github.com>
2022-02-08 18:45:05 +0530

    Update radiusandfile.py

--- radiusandfile.py
+++ radiusandfile.py
@@ -1,6 +1,11 @@
 >>>r=int(input('enter the radius value='))
-enter the radius value=2
+enter the radius value=1.1
 >>> pi=3.14
 >>> area=pi*r*r
 >>> print('area of circle is=',area)
-area of circle is=12.56
+area of circle is=3.7994
+>>> filename=input('enter file name =')
+enter file name = abc.py
+>>> f_exts=filename.split(".")
+>>> print('extension of file is'+repr(f_exts[-1]))
+extension of file is'py'
