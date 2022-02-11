ad80deac8bf57e5217b404dbf7b54e2aff977661
Muskan-coder719 <82265381+Muskan-coder719@users.noreply.github.com>
2022-02-11 15:46:30 +0530

    Create positivenumber.py

---
+++ positivenumber.py
@@ -0,0 +1,10 @@
+list1=[23,30,-9,45,-43]
+list2=[12,-34,52,32,-37]
+print("positive numbers in list1 is :")
+for i in list1 :
+    if i>=0:
+       print(i)
+print("\npositive numbers in  list2 is :" )
+for j in list2 :
+    if j>=0:
+        print(j)
