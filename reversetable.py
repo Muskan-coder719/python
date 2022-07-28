0a6e3651016cf85ba048f46702929e578d63bf27
Muskan-coder719 <82265381+Muskan-coder719@users.noreply.github.com>
2022-07-28 12:43:44 +0530

    Update reversetable.py

--- reversetable.py
+++ reversetable.py
@@ -1,3 +1,16 @@
 number = int(input("Enter the number: "))
 for i in range(-10,0):
     print(f"{number} X {i*-1}=" ,number*(i*-1))
+
+    
+OUTPUT :
+5 X 10= 50
+5 X 9= 45
+5 X 8= 40
+5 X 7= 35
+5 X 6= 30
+5 X 5= 25
+5 X 4= 20
+5 X 3= 15
+5 X 2= 10
+5 X 1= 5
