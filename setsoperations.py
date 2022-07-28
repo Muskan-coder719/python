a23ec3172318ffd6b0236babf58b583c3fc01e3a
Muskan-coder719 <82265381+Muskan-coder719@users.noreply.github.com>
2022-07-28 12:41:02 +0530

    Create setsoperations.py

---
+++ setsoperations.py
@@ -0,0 +1,13 @@
+s1={1,2,3,4}
+s2={2,4,6,8}
+print("the union of s1 and s2 is :",s1|s2)
+print("the intersection of s1 and s2 is :",s1 & s2)
+print("the difference of s1 and s2 is :",s1-s2)
+print("the assymetric diffrence of s1 and s2 is :",s1^s2)
+
+
+OUTPUT :
+the union of s1 and s2 is : {1, 2, 3, 4, 6, 8}
+the intersection of s1 and s2 is : {2, 4}     
+the difference of s1 and s2 is : {1, 3}       
+the assymetric diffrence of s1 and s2 is : {1, 3, 6, 8}
